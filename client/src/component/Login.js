540895fdfe9867f060a06956055fc1b4e9e1292b
Ajeethkumar26 <113531557+Ajeethkumar26@users.noreply.github.com>
2023-07-08 18:53:42 +0530

    Add files via upload

---
+++ client/src/component/Login.js
@@ -0,0 +1,87 @@
+
+import React, { useState } from 'react';
+import axios from 'axios';
+import { Route, Routes, useNavigate } from 'react-router-dom';
+
+export const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    Login({ username, password });
+  };
+  const Login = async () => {
+  let  formData = {
+    username,
+    password
+  }
+    try {
+      // Make a POST request to your backend API with the login credentials
+      const response = await axios.post('http://localhost:8000/users/login/', formData); 
+  
+      // Check if the login was successful based on the response from the server
+      if (response.status==200) {
+        // Once authenticated, set `isLoggedIn` to true
+      // setIsLoggedIn(true);
+      console.log('Login sucesss:', response.data.message);
+      localStorage.setItem('isauth','true');
+      navigate('/TextDisplayPanel', { replace: true });
+
+      } else {
+        // Handle unsuccessful login (e.g., display error message)
+        console.log('Login failed:', response.data.message);
+      }
+    } catch (error) {
+      console.error('Error occurred during login:', error);
+    }
+  };
+  
+  const handleLogout = async () => {
+    try {
+      // Make a POST request to your backend API to perform the logout
+      const response = await axios.post('http://localhost:8000/users/logout/'); // Replace with your backend URL
+  
+      // Check if the logout was successful based on the response from the server
+      if (response.data.success) {
+        // Once logged out, set `isLoggedIn` to false
+        // setIsLoggedIn(false);
+      } else {
+        // Handle unsuccessful logout (e.g., display error message)
+        console.log('Logout failed:', response.data.message);
+      }
+    } catch (error) {
+      console.error('Error occurred during logout:', error);
+    }
+  };
+  
+
+  return (
+    <form className="login-form" onSubmit={handleSubmit}>
+      <h2>Login</h2>
+      <input
+        type="text"
+        placeholder="Username"
+        value={username}
+        onChange={handleUsernameChange}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={handlePasswordChange}
+      />
+      <button onClick={Login}>Login</button>
+    </form>
+  );
+};
+
+export default Login;
---
+++ client/src/component/navigation.js
@@ -0,0 +1,18 @@
+
+import React, { useState, useEffect} from 'react';
+export function Navigation() {
+   
+    // let isauth=localStorage.getItem('isauth')
+    
+     return ( 
+        <div>
+            {/* {
+          isauth==true?(
+          <TextDisplayPanel></TextDisplayPanel>
+          ):(<Login>
+  
+              </Login>)} */}
+         
+         </div>
+       );
+}
